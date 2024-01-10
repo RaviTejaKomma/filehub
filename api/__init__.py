b928fdffaba0dd903d64ccb9be1df4c9699a783d
RaviTejaKomma <ravieee929374s@gmail.com>
2024-01-10 19:20:10 +0530

    chore: removed some redundant code

--- api/__init__.py
+++ api/__init__.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from api.files import files_api_v1
 
 
-def create_app(test_config=None):
+def create_app():
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     CORS(app)
